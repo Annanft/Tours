89a240c471e438757fab3a2fb0b47edf96227bef
Anna Hutik <annagutnik84@gmail.com>
2023-04-09 16:54:38 +0200

    add loading component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { AllTours } from "./components/AllTours";
+import { Loading } from "./components/Loading";
 
 
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [tours, setTours] = useState([])
+  const [loading, setLoading] = useState(true)
+
 
   useEffect(() => {
-axios.get(url).then((res)=> setTours(res.data))
+    const fetchData = async()=>{
+      const response = await fetch(url)
+      const items = await response.json()
+      setTours(items)
+      setLoading(false)
+
+    }
+    fetchData()
+
   },[])
+
+  if(loading){
+    return <Loading />
+  }
+
 
   const filterTour = (tit) => setTours(tours.filter((el)=> el.name !== tit))
   
   if(tours.length ===0){
-    return<div>
+    return<div className="wrapper">
       <h1>No tours left</h1>
       <button onClick={() => axios.get(url).then((res)=> setTours(res.data))}>Reset tours</button>
     </div>
@@ -27,7 +43,8 @@ axios.get(url).then((res)=> setTours(res.data))
 
   return (
     <div className="wrapper">
-      <AllTours tours={tours} filterTour={filterTour}/> 
+      <AllTours tours={tours} filterTour={filterTour}/>
+       
       
     
     </div>
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+
+export const Loading = () => {
+   
+  return (
+    <div className='load'></div>
+  )
+}
